8c27dbfe9b6c01f790d81c2c628abe54051c5983
E.G. ismaail <contact@ismaail.com>
2015-03-15 22:41:04 +0000

    Add Acl is allowed test

--- src/User/test/UserTest/Authorization/AclTest.php
+++ src/User/test/UserTest/Authorization/AclTest.php
@@ -33,4 +33,60 @@ class AclTest extends PHPUnit_Framework_TestCase
             'resources' => [],
         ]);
     }
+
+    public function testIsAllowedRules()
+    {
+        $acl = new Acl($this->getAclConfig());
+
+        $this->assertTrue(
+            $acl->isAllowed('GUEST', 'Acme::FirstController'),
+            'GUEST is allowed inAcme::SecondController'
+        );
+
+        $this->assertTrue(
+            $acl->isAllowed('GUEST', 'Acme::SecondController'),
+            'GUEST is allowed in Acme::SecondController'
+        );
+
+        $this->assertFalse(
+            $acl->isAllowed('GUEST', 'Acme'),
+            'GUEST is not allowed in Acme namespace'
+        );
+
+        $this->assertTrue(
+            $acl->isAllowed('MEMBER', 'Acme'),
+            'MEMBER is allowed in Acme namespace'
+        );
+    }
+
+    private function getAclConfig()
+    {
+        $config = array(
+            'default_role' => 'GUEST',
+            'roles'        => [
+                'GUEST'  => null,
+                'MEMBER' => 'GUEST',
+            ],
+            'resources' => [
+                'deny' => [
+                    'all' => [
+                        'all' => 'GUEST',
+                    ]
+                ],
+                'allow' => [
+                    'Acme::FirstController' => [
+                        'all' => 'GUEST',
+                    ],
+                    'Acme::SecondController' => [
+                        'all' => 'GUEST',
+                    ],
+                    'Acme' => [
+                        'all' => 'MEMBER',
+                    ]
+                ],
+            ],
+        );
+
+        return $config;
+    }
 }
