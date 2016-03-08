7daa506ed644fa22aae6560f9e088ea4fb403c12
E.G. ismaail <contact@ismaail.com>
2016-03-08 15:34:19 +0000

    Fix getting ServiceLoactor in plugin AuthService

--- src/User/Controller/Plugin/AuthService.php
+++ src/User/Controller/Plugin/AuthService.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace User\Controller\Plugin;
 
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
 
 /**
  * Class AuthService
  * @package User\Controller\Plugin
  */
-class AuthService extends AbstractPlugin
+class AuthService extends AbstractPlugin implements ServiceLocatorAwareInterface
 {
     /**
      * @var \User\Authentication\Auth
@@ -15,12 +18,33 @@ class AuthService extends AbstractPlugin
     protected $authService;
 
     /**
+     * @var ServiceLocatorInterface
+     */
+    private $serviceLocator;
+
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
+    }
+
+    /**
+     * @return ServiceLocatorInterface|\Zend\Mvc\Controller\PluginManager
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+
+    /**
      * @return \User\Authentication\Auth
      */
     public function __invoke()
     {
         if (! $this->authService) {
-            $this->authService = $this->getController()->getServiceLocator()->get('AuthService');
+            $this->authService = $this->getServiceLocator()->getServiceLocator()->get('AuthService');
         }
 
         return $this->authService;
